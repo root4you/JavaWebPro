c4ac27488e44d38e24ee7463c926ebea1a0ca176
Jung Hwa Ahn <58136894+root4you@users.noreply.github.com>
2020-02-29 22:59:42 +0900

    Update AddServlet.java

--- Study01/src/unit01/AddServlet.java
+++ Study01/src/unit01/AddServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//@WebServlet("/add")
+
 public class AddServlet extends HttpServlet{
 @Override
 protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
